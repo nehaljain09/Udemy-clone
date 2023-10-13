75043978b5281e2e03344a970a43037e12accb24
nehaljain09 <nehal.jain@ril.com>
2023-10-13 20:37:36 +0530

    UI & functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import { MdMenu, MdShoppingCart } from "react-icons/md";
 import { Link } from "react-router-dom";
-// import { useSidebarContext } from "../context/sidebar_context";
-// import { useCartContext } from "../context/cart_context";
+import { useSidebarContext } from "../context/sidebar_context";
+import { useCartContext } from "../context/cart_context";
 
 const Navbar = () => {
-  // const { total_items } = useCartContext();
-  // const { openSidebar } = useSidebarContext();
+  const { total_items } = useCartContext();
+  const { openSidebar } = useSidebarContext();
 
   return (
     <NavbarWrapper className="bg-white flex">
       <div className="container w-100">
         <div className="brand-and-toggler flex flex-between w-100">
           <Link to="/" className="navbar-brand text-uppercase ls-1 fw-8">
-            <span>c</span>oursean
+            <span>u</span>demy
           </Link>
 
           <div className="navbar-btns flex">
             <Link to="/cart" className="cart-btn">
               <MdShoppingCart />
-              <span className="item-count-badge">0</span>
+              <span className="item-count-badge">{total_items}</span>
             </Link>
             <button
               type="button"
               className="sidebar-open-btn"
-              // onClick={() => openSidebar()}
+              onClick={() => openSidebar()}
             >
               <MdMenu />
             </button>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
+import CoursesList from "../components/CourseList";
+import CategoriesList from "../components/CategoriesList";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="holder">
+      <Hero />
+      <CoursesList />
+      <CategoriesList />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
